3e7f3be3d8ad3a3dd51b1af3ff0a6eeb8d1bf131
dahe <649761355@qq.com>
2019-05-14 13:51:03 +0800

    update

---
+++ WPF快速开发框架/DaHeDemo/Model/SampleModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaHeDemo.Model
+{
+    public class SampleModel:MVVM.MvvmBase
+    {
+        private string _name;
+        /// <summary>
+        /// 文件上传的名字 preview
+        /// </summary>
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                RaisePropertyChanged(nameof(Name));
+            }
+        }
+    }
+}
---
+++ WPF快速开发框架/DaHeDemo/ViewModel/SampleViewModel.cs
@@ -0,0 +1,53 @@
+﻿using DaHeDemo.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DaHeDemo.ViewModel
+{
+    public class SampleViewModel:MVVM.MvvmBase
+    {
+        public SampleViewModel()
+        {
+
+        }
+        #region 属性
+        Model.SampleModel _SampleModel;
+        public Model.SampleModel SampleModel
+        {
+            get { return _SampleModel; }
+            set
+            {
+                _SampleModel = value;
+                RaisePropertyChanged(nameof(SampleModel));
+            }
+        }
+
+        #endregion
+
+        #region 命令
+
+        /// <summary>
+        /// 取消点击
+        /// </summary>    
+        public ICommand BtnCancel => new DelegateCommand(obj =>
+        {
+           // (obj as Views.Shutdown).Close();
+        });
+        /// <summary>
+        /// 确认点击
+        /// </summary>
+        public ICommand BtnConfirm => new DelegateCommand(obj =>
+        {
+            //写入缓存中的配置
+           // MessageBox.Show("确触发");
+          //  (obj as Views.Shutdown).Close();
+        });
+
+
+        #endregion
+    }
+}
